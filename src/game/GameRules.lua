f4de0fe21faa95317b27f81e2294fc661726ef0b
Yan Kalbaska <yan@kalbaska.net>
2023-08-24 01:09:46 +0300

    add placeholder object view pair

--- src/game/GameRules.lua
+++ src/game/GameRules.lua
@@ -1,9 +1,10 @@
+local Placeholder = require "objects.Placeholder"
 -- [Require all added game objects for game world]
 
 --- All available game object types for easier by-type iteration during updates
 ---@alias GameObjectType
----| 'Default' # a placeholder set
--- [Define all game object types]
+---| 'Placeholder' # a placeholder set
+-- [Define all game object types as an enumeration]
 
 --- Type-associative table of indexed game objects, e.g. {objectType1 = {object1, object2}, ...}.<br> For autocompletion to work properly when specific objects are fetched, requires casting to a type `---@cast object1 GameObjectType`.
 ---@alias GameObjectCollection { [GameObjectType]: GameObject[] } 
@@ -19,6 +20,10 @@ function GameRules:createWorld()
     local gameWorld = {}
 
     -- [Create, configure game objects and add them to gameWorld]
+    gameWorld.Placeholder = {}
+    local aPlaceholder = Placeholder:new()
+    -- [Configure game object]
+    table.insert(gameWorld.Placeholder, aPlaceholder)
 
     return gameWorld
 end
@@ -27,7 +32,11 @@ end
 ---@param timediff number Game world time difference from previous call
 function GameRules:updateWorld(gameWorld, timediff)
 
-    -- [Update objects of gameWorld]
+    -- [Update all object types of gameWorld]
+    for _, placeholder in ipairs(gameWorld.Placeholder) do
+        ---@cast placeholder Placeholder
+        placeholder:updateWith(timediff)
+    end
 
 end
 
