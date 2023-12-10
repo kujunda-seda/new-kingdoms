4897c3354e64fa6fc604d88d5b13b9087c493dd7
Yan Kalbaska <yan@kalbaska.net>
2023-12-10 16:50:04 +0300

    adding a car

--- src/objects/PlaceholderObject.lua
+++ src/objects/PlaceholderObject.lua
@@ -1,35 +1,43 @@
 local GameObject = require "types.GameObject"
 
---- `PlaceholderObject` game object is used to show an example game object only. Can be removed
----@class PlaceholderObject:GameObject
-local PlaceholderObject = GameObject:new {}
+--- Game object is responsible for car logic
+---@class Car:GameObject
+---@field roadPosition number Fraction of relative position on the road
+---@field speed number Current car speed
+---@field isActivated boolean
+local Car = GameObject:new {}
 
 --- Constants
 
----@return PlaceholderObject
-function PlaceholderObject:new()
+local START_OF_ROAD = 0
+local END_OF_ROAD = 1
+local MOVEMENTS_PER_SEC = 100
+
+---@return Car
+function Car:new()
     -- Required code for instances to find defined methods and inheritance
     local newObject = setmetatable({}, self)
     self.__index = self
 
-    -- [Configure new object]
+    newObject.roadPosition = START_OF_ROAD
+    newObject.speed = 0
+    newObject.isActivated = false
 
     return newObject
 end
 
---- Updates an example object
+--- Moves car according to its current speed and repositions it at the beginning of the road
 ---@param time number Game timing difference from previous update
-function PlaceholderObject:updateWith(time)
-
-    -- [Update logic]
-
+function Car:moveWith(time)
+    self.roadPosition = self.roadPosition + self.speed * time / MOVEMENTS_PER_SEC
+    if self.roadPosition > END_OF_ROAD then
+        self.roadPosition = START_OF_ROAD
+    end
 end
 
 --- Notifies object of interaction
-function PlaceholderObject:objectInteracted()
-
-    -- [Interaction logic]
-
+function Car:objectInteracted()
+    self.isActivated = not self.isActivated
 end
 
-return PlaceholderObject
+return Car
