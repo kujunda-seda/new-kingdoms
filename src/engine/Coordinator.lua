69732c87c521017587377825f3d856eb29b843c3
Yan Kalbaska <yan@kalbaska.net>
2025-07-18 10:52:17 +0300

    restructure folders and rename classes. closes #29

--- src/engine/Coordinator.lua
+++ src/engine/Coordinator.lua
@@ -1,21 +1,21 @@
-local Engine = require "game.Engine"
-local GameLayout = require "presenter.GameLayout"
+local Loop = require "engine.Loop"
+local GameLayout = require "game.GameLayout"
 local GameController = require "game.GameController"
 
 --- Coordinates game objects' run loop, visual properties, and interaction.
----@class Presenter
----@field private engine Engine Game loop coordinator
+---@class Coordinator
+---@field private loop Loop Main game loop
 ---@field private layout GameLayout Game view coordinator
 ---@field private controller GameController Game (touch) interaction
 ---@field private viewHierarchy ViewPair[] A z-indexed array of views
-local Presenter = {}
+local Coordinator = {}
 
----@return Presenter
-function Presenter:new()
+---@return Coordinator
+function Coordinator:new()
     -- Lua code to find object & inherited methods (tinyurl.com/oop-lua)
     local newObject = setmetatable({}, self); self.__index = self
 
-    newObject.engine = Engine:new()
+    newObject.loop = Loop:new()
     newObject.layout = GameLayout:new()
     newObject.controller = GameController:new()
     newObject.viewHierarchy = {}
@@ -23,21 +23,21 @@ function Presenter:new()
     return newObject
 end
 
-function Presenter:startEngine()
+function Coordinator:startEngine()
     local layoutViews = function()
-        local objects = self.engine:getViewableObjects()
+        local objects = self.loop:getViewableObjects()
         self.viewHierarchy = self.layout:layoutObjectsIntoViewHierarchy(objects)
     end
 
-    self.engine:startWithObjectListener(layoutViews)
+    self.loop:startWithObjectListener(layoutViews)
 end
 
-function Presenter:stopEngine()
-    self.engine:stop()
+function Coordinator:stopEngine()
+    self.loop:stop()
 end
 
 --- Renders previously layouted views in UI based on their z-index.
-function Presenter:drawViewHierarchy()
+function Coordinator:drawViewHierarchy()
     for _, viewPair in ipairs(self.viewHierarchy) do  -- cycle through view hierarchy
         viewPair.view:draw()
     end
@@ -45,14 +45,14 @@ end
 
 --- Time event from UI framework.
 ---@param dt number Interval after previous event in milliseconds
-function Presenter:timeEvent(dt)
-    self.engine:timePassed(dt)  -- use game time scale
+function Coordinator:timeEvent(dt)
+    self.loop:timePassed(dt)  -- use game time scale
 end
 
 --- Check touch attribution with z-indexed hierarchy responder chain.
 ---@param x number mouse click or touch x-coordinate
 ---@param y number mouse click or touch y-coordinate
-function Presenter:attributeTouch(x, y)
+function Coordinator:attributeTouch(x, y)
     for _, viewPair in ipairs(self.viewHierarchy) do  -- cycle through z-indexed view hierarchy
         if viewPair.view:touchInside(x, y) then
             if self.controller:processTouchFor(viewPair, x, y) then
@@ -62,4 +62,4 @@ function Presenter:attributeTouch(x, y)
     end
 end
 
-return Presenter
+return Coordinator
--- src/engine/Loop.lua
+++ src/engine/Loop.lua
@@ -2,13 +2,13 @@ local GameRules = require "game.GameRules"
 local GameObjectCollection = require "types.GameObjectCollection"
 
 --- Manages main game engine loop (start, update, stop).
----@class Engine
+---@class Loop
 ---@field private gameWorld GameObjectCollection All objects of the game universe.
 ---@field private gameObjectsChanged function? Callback to react on changes.
-local Engine = {}
+local Loop = {}
 
----@return Engine
-function Engine:new()
+---@return Loop
+function Loop:new()
     -- Lua code to find object & inherited methods (tinyurl.com/oop-lua)
     local newObject = setmetatable({}, self); self.__index = self
 
@@ -20,22 +20,22 @@ end
 
 --- Starts a game engine and assigns callback to be called when objects change.
 ---@param listener function Callback function to be called when relayout is required
-function Engine:startWithObjectListener(listener)
+function Loop:startWithObjectListener(listener)
     self.gameWorld = GameRules:createWorld()
     self.gameObjectsChanged = listener
 end
 
-function Engine:stop()
+function Loop:stop()
     self.gameObjectsChanged = nil
 end
 
 --- Triggers game engine time shift.
 ---@param dt number Time difference from previous similar event in game time
-function Engine:timePassed(dt)
+function Loop:timePassed(dt)
     -- default scenario: platform time = game time
     GameRules:updateWorld(self.gameWorld, dt)
 
-    -- run callback to presenter with each time increment
+    -- run callback to coordinator with each time increment
     if self.gameObjectsChanged ~= nil then
         self:gameObjectsChanged()
     end
@@ -43,9 +43,9 @@ end
 
 --- Returns viewable game objects grouped by type for easier parsing.
 ---@return GameObjectCollection
-function Engine:getViewableObjects()
+function Loop:getViewableObjects()
     -- default scenario: returns all objects of the universe
     return self.gameWorld
 end
 
-return Engine
+return Loop
